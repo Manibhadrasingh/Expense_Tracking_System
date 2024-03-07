dd0d4bdee5cee42b7e1d9156ae948fd6d4a515dc
Manibhadra-Singh <119622801+Manibhadrasingh@users.noreply.github.com>
2024-03-07 15:52:01 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h2>
+     Price Tag Tracker
+    </h2>
+  )
+}
+
+export default Header
---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,43 @@
+import React , {createContext ,  useReducer} from 'react'
+import AppReducer from './AppReducer';
+const initialState = {
+    transactions : []
+}
+
+// cretae context
+
+export const GlobalContext = createContext(initialState);
+
+
+export const GlobalProvider = ({children}) =>{
+    const [state, dispatch] = useReducer(AppReducer,initialState);
+
+    // delete transaction
+    function deleteTransaction(id){
+        dispatch({
+            type:'DELETE_TRANSACTION',
+            payload : id
+        });
+    }
+
+    // add transaction
+
+    function addTransaction(transaction){
+        dispatch({
+            type:'ADD_TRANSACTION',
+            payload : transaction
+        })
+    }
+
+    return (
+    <GlobalContext.Provider value={
+        {
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
+        }
+    }> 
+        {children}
+    </GlobalContext.Provider>
+    )
+}
